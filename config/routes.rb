332b676d8c653cc668880330cba96fb77c046228
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2020-08-18 20:37:12 -0400

    Setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'articles#index'
-  resources :articles, except: :index
+  root to: 'cocktails#index'
   
   resources :cocktails, only: [ :index, :new, :create, :show ] do
     resources :doses, only: [ :new, :create ]
